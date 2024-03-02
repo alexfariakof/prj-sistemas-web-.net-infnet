0e35130dd94454cdaef54bff20297a54f7e9753b
Ribeiro <alexfariakof@gmail.com>
2024-03-02 14:04:53 -0300

    Update coverage_report.ps1

--- XUnitTest/coverage_report.ps1
+++ XUnitTest/coverage_report.ps1
@@ -17,5 +17,5 @@ if (-not (Test-Path $projectAngular\node_modules)) {
 }
 
 # Executa Teste Unitários e gera o relatório de cobertura do Frontend 
-npm run test:coverage
+npm run test:coverage > $null 2>&1
 
