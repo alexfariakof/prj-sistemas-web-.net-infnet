5a08f75ff2ccbebc7ed22401f6202e42d7e72f7b
Ribeiro <alexfariakof@gmail.com>
2024-03-10 23:06:07 -0300

    Navigation Pages Home/Login/Create Account

--- AngularApp/src/app/app.routing.module.ts
+++ AngularApp/src/app/app.routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { CustomerComponent } from './pages/customer/customer.component';
+import { MerchantComponent } from './pages/merchant/merchant.component';
 
 export const routes: Routes = [
     { path: '',  pathMatch: 'full', redirectTo: 'home' },
     { path: 'home', component: HomeComponent },
-   { path: "login", component: LoginComponent},
+    { path: "login", component: LoginComponent},
+    { path: "cliente", component: CustomerComponent},
+    { path: "comerciante", component: MerchantComponent},
 ];
 
 @NgModule({
--- AngularApp/src/app/pages/login/login.component.ts
+++ AngularApp/src/app/pages/login/login.component.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Login } from 'src/app/model';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, MatFormFieldModule],
+  imports: [CommonModule, MatFormFieldModule, ReactiveFormsModule, MatInputModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -22,8 +23,8 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
     this.loginForm = this.formbuilder.group({
-      email: ['teste@teste.com', [Validators.required, Validators.email]],
-      senha: ['12345T!', Validators.required]
+      email: ['user@custumer.com', [Validators.required, Validators.email]],
+      senha: ['12345', Validators.required]
     }) as FormGroup & Login;
   }
 
