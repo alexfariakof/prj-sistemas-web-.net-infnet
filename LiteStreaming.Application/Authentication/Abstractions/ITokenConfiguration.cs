b2eb7953585a3a810c8009e092d1010b47410a07
Alex Ribeiro de Faria <42475620+alexfariakof@users.noreply.github.com>
2024-05-27 03:30:47 -0300

    Update ITokenConfiguration.cs

--- LiteStreaming.Application/Authentication/Abstractions/ITokenConfiguration.cs
+++ LiteStreaming.Application/Authentication/Abstractions/ITokenConfiguration.cs
@@ -1,8 +1,8 @@
 ﻿namespace Application.Authentication.Abstractions;
 public interface ITokenConfiguration
 {
-    public string Audience { get; set; }
-    public string Issuer { get; set; }
+    public string? Audience { get; set; }
+    public string? Issuer { get; set; }
     public int Seconds { get; set; }
     public int DaysToExpiry { get; set; }
     string GenerateRefreshToken();
