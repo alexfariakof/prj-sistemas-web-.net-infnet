d06dccf888a3f72a829ffc5a4ead7f463628b278
Alex Ribeiro de Faria <42475620+alexfariakof@users.noreply.github.com>
2024-06-21 07:39:52 -0300

    Delete IUserService.cs

--- LiteStreaming.Application/Administrative/Interfaces/IUserService.cs
+++
@@ -1,8 +0,0 @@
-﻿using Application.Administrative.Dto;
-using Application.Shared.Dto;
-
-namespace Application.Administrative.Interfaces;
-public interface IAuthenticationService
-{
-    AdministrativeAccountDto Authentication(LoginDto dto);
-}
