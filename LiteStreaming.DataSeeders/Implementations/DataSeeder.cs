2ebc35645133373f0aeb687769efdaa090f1795d
Alex Ribeiro de Faria <42475620+alexfariakof@users.noreply.github.com>
2024-06-28 04:22:26 -0300

    Entrega Projeto Persistência e MVC [24E2_2]

--- LiteStreaming.DataSeeders/Implementations/DataSeeder.cs
+++ LiteStreaming.DataSeeders/Implementations/DataSeeder.cs
@@ -1,35 +1,40 @@
 ﻿using Repository;
 
 namespace DataSeeders;
+
 public class DataSeeder : IDataSeeder
 {
     private readonly RegisterContext _context;
+
     public DataSeeder(RegisterContext context)
     {
         _context = context;
     }
+
     public void SeedData()
     {
         try
         {
-            if (_context.Database.CanConnect())
+            if (_context.Database.EnsureCreated())
+            {
+                Console.WriteLine("Banco de dados foi criado.");
+
+                new DataSeederFlat(_context).SeedData();
+                new DataSeederGenre(_context).SeedData();
+                new DataSeederBand(_context).SeedData();
+                new DataSeederUserType(_context).SeedData();
+                new DataSeederCustomer(_context).SeedData();
+                new DataSeederMerchant(_context).SeedData();
+                new DataSeederMusic(_context).SeedData();
+            }
+            else
             {
                 Console.WriteLine("Banco de dados já existe. Não será recriado.");
-                return;
             }
-            _context.Database.EnsureDeleted();
-            _context.Database.EnsureCreated();
-
-            new DataSeederFlat(_context).SeedData();
-            new DataSeederGenre(_context).SeedData();
-            new DataSeederBand(_context).SeedData();
-            new DataSeederUserType(_context).SeedData();
-            new DataSeederCustomer(_context).SeedData();
-            new DataSeederMerchant(_context).SeedData();
-            new DataSeederMusic(_context).SeedData();
         }
-        catch
+        catch (Exception ex)
         {
+            Console.WriteLine($"Erro ao verificar ou criar o banco de dados: {ex.Message}");
             throw;
         }
     }
