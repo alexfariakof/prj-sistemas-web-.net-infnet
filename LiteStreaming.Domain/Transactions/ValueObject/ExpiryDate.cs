27f0acb92f4e65fdf22d35adecc6f16f58a05820
Alex Ribeiro de Faria <42475620+alexfariakof@users.noreply.github.com>
2024-06-27 21:36:11 -0300

    Implementação corretiva de recursividade em loop

--- LiteStreaming.Domain/Transactions/ValueObject/ExpiryDate.cs
+++ LiteStreaming.Domain/Transactions/ValueObject/ExpiryDate.cs
@@ -5,20 +5,29 @@ public record ExpiryDate
 {
     public static implicit operator (int Month, int Year)(ExpiryDate expiryDate) => (expiryDate.Month, expiryDate.Year);
     public static implicit operator ExpiryDate((int Month, int Year) values) => new ExpiryDate(values);
+
     public DateTime Value { get; set; }
-    
+
     [NotMapped]
     public int Month { get; init; }
-    
+
     [NotMapped]
     public int Year { get; init; }
-   
+
     public ExpiryDate(DateTime value)
     {
         Value = value;
         Month = value.Month;
         Year = value.Year;
     }
+
+    public ExpiryDate((int Month, int Year) values)
+    {
+        Month = values.Month;
+        Year = values.Year;
+        Value = new DateTime(Year, Month, 1);
+    }
+
     public string Formatted_ptBr()
     {
         return $"{Value.Month:D2}/{Value.Year % 100:D2}";
