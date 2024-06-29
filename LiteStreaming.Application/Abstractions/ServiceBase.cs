ef11a49a4a8561b6cdb1caa676e77693360b3f20
Alex Ribeiro de Faria <42475620+alexfariakof@users.noreply.github.com>
2024-06-29 06:15:12 -0300

    Refactor Method to Rreduce Cognitive Complexity

--- LiteStreaming.Application/Abstractions/ServiceBase.cs
+++ LiteStreaming.Application/Abstractions/ServiceBase.cs
@@ -14,7 +14,7 @@ public abstract class ServiceBase<Dto, Entity> where Dto : class, new() where En
     }
     public abstract Dto Create(Dto obj);
     public abstract List<Dto> FindAll();
-    public virtual List<Dto> FindAll(string sortProperty = "", SortOrder sortOrder = 0) { throw new NotImplementedException("Sort FindAll is not implemented"); }
+    public virtual List<Dto> FindAll(string sortProperty = null, SortOrder sortOrder = 0) { throw new NotImplementedException("Sort FindAll is not implemented"); }
     public abstract Dto FindById(Guid id);
     public abstract Dto Update(Dto obj);
     public abstract bool Delete(Dto obj);
--- LiteStreaming.Repository/Abstractions/BaseRepository.cs
+++ LiteStreaming.Repository/Abstractions/BaseRepository.cs
@@ -98,11 +98,11 @@ public abstract class BaseRepository<T> where T : class, new()
     /// <param name="sortProperty">A propriedade para ordenar. Se nula, a primeira propriedade pública é usada.</param>
     /// <param name="sortOrder">A ordem para classificar as entidades.</param>
     /// <returns>Uma coleção ordenada de entidades.</returns>
-    public virtual IEnumerable<T> FindAll(string sortProperty = "", SortOrder sortOrder = SortOrder.Ascending)
+    public virtual IEnumerable<T> FindAll(string sortProperty = null, SortOrder sortOrder = SortOrder.Ascending)
     {
         var list = Context.Set<T>().ToList();
 
-        if (String.IsNullOrEmpty(sortProperty))
+        if (sortProperty is null)
             return list;
 
         PropertyInfo? prop = null;
@@ -160,14 +160,8 @@ public abstract class BaseRepository<T> where T : class, new()
 
         // Ordena a lista com base na expressão de acesso à propriedade
         if (sortOrder == SortOrder.Ascending)
-        {
-            list = list.AsQueryable().OrderBy(sortExpression).ToList();
-        }
+            return list.AsQueryable().OrderBy(sortExpression).ToList();
         else
-        {
-            list = list.AsQueryable().OrderByDescending(sortExpression).ToList();
-        }
-
-        return list;
+            return list.AsQueryable().OrderByDescending(sortExpression).ToList();
     }
 }
