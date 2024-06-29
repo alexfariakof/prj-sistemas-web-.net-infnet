b62b2b1d885f73293db536707a6032cacb9954a0
Alex Ribeiro de Faria <42475620+alexfariakof@users.noreply.github.com>
2024-06-29 03:37:35 -0300

    Implementação  e Documentação   da Classe BaseRepository com Sort das Propriedades Publicas e herdadas

--- LiteStreaming.Repository/Abstractions/BaseRepository.cs
+++ LiteStreaming.Repository/Abstractions/BaseRepository.cs
@@ -4,72 +4,146 @@ using Microsoft.Data.SqlClient;
 using System.Reflection;
 
 namespace Repository.Abstractions;
+
+/// <summary>
+/// Repositório base que fornece operações CRUD comuns para entidades.
+/// </summary>
+/// <typeparam name="T">Tipo da entidade.</typeparam>
 public abstract class BaseRepository<T> where T : class, new()
 {
     private DbContext Context { get; set; }
 
+    /// <summary>
+    /// Inicializa uma nova instância da classe <see cref="BaseRepository{T}"/>.
+    /// </summary>
+    /// <param name="context">O contexto do banco de dados.</param>
     protected BaseRepository(DbContext context)
     {
         Context = context;
     }
 
+    /// <summary>
+    /// Salva a entidade especificada no banco de dados.
+    /// </summary>
+    /// <param name="entity">A entidade a ser salva.</param>
     public virtual void Save(T entity)
     {
         Context.Add(entity);
         Context.SaveChanges();
     }
 
+    /// <summary>
+    /// Atualiza a entidade especificada no banco de dados.
+    /// </summary>
+    /// <param name="entity">A entidade a ser atualizada.</param>
     public virtual void Update(T entity)
     {
         Context.Update(entity);
         Context.SaveChanges();
     }
 
+    /// <summary>
+    /// Exclui a entidade especificada do banco de dados.
+    /// </summary>
+    /// <param name="entity">A entidade a ser excluída.</param>
     public virtual void Delete(T entity)
     {
         Context.Remove(entity);
         Context.SaveChanges();
     }
 
-    public virtual IEnumerable<T> GetAll(string sortProperty = null, SortOrder sortOrder = SortOrder.Ascending)
+    /// <summary>
+    /// Obtém a entidade pelo identificador Guid especificado.
+    /// </summary>
+    /// <param name="id">O identificador Guid.</param>
+    /// <returns>A entidade com o identificador Guid especificado.</returns>
+    public virtual T GetById(Guid id)
+    {
+        return Context.Set<T>().Find(id) ?? new();
+    }
+
+    /// <summary>
+    /// Obtém a entidade pelo identificador inteiro especificado.
+    /// </summary>
+    /// <param name="id">O identificador inteiro.</param>
+    /// <returns>A entidade com o identificador inteiro especificado.</returns>
+    public virtual T GetById(int id)
+    {
+        return Context.Set<T>().Find(id) ?? new();
+    }
+
+    /// <summary>
+    /// Encontra entidades que correspondem à expressão especificada.
+    /// </summary>
+    /// <param name="expression">A expressão para filtrar entidades.</param>
+    /// <returns>Uma coleção de entidades que correspondem à expressão especificada.</returns>
+    public virtual IEnumerable<T> Find(Expression<Func<T, bool>> expression)
+    {
+        return Context.Set<T>().Where(expression);
+    }
+
+    /// <summary>
+    /// Verifica se existem entidades que correspondem à expressão especificada.
+    /// </summary>
+    /// <param name="expression">A expressão para filtrar entidades.</param>
+    /// <returns><c>true</c> se existirem entidades que correspondem à expressão especificada; caso contrário, <c>false</c>.</returns>
+    public virtual bool Exists(Expression<Func<T, bool>> expression)
+    {
+        return Find(expression).Any();
+    }
+
+    /// <summary>
+    /// Encontra todas as entidades e as ordena pela propriedade especificada.
+    /// </summary>
+    /// <param name="sortProperty">A propriedade para ordenar. Se nula, a primeira propriedade pública é usada.</param>
+    /// <param name="sortOrder">A ordem para classificar as entidades.</param>
+    /// <returns>Uma coleção ordenada de entidades.</returns>
+    public virtual IEnumerable<T> FindAll(string sortProperty = null, SortOrder sortOrder = SortOrder.Ascending)
     {
         var list = Context.Set<T>().ToList();
+
+        if (String.IsNullOrEmpty(sortProperty))
+            return list;
+
         PropertyInfo? prop = null;
         Expression<Func<T, object>>? sortExpression = null;
 
-        // Por default, caso a sortProperty seja null, é setada a primeira propriedade pública da entidade
-        if (!string.IsNullOrWhiteSpace(sortProperty))
-        {
-            prop = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                            .FirstOrDefault(p => string.Equals(p.Name, sortProperty, StringComparison.OrdinalIgnoreCase));
+        prop = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                        .FirstOrDefault(p => string.Equals(p.Name, sortProperty, StringComparison.OrdinalIgnoreCase));
 
-            // Se a propriedade não for encontrada diretamente na entidade T, percorre as propriedades de navegação
-            if (prop == null)
+        // Se a propriedade não for encontrada diretamente na entidade T, percorre as propriedades de navegação
+        if (prop == null)
+        {
+            var navigations = Context.Model.FindEntityType(typeof(T))?.GetNavigations();
+            if (navigations != null)
             {
-                var navigations = Context.Model.FindEntityType(typeof(T))?.GetNavigations();
-                if (navigations != null)
+                foreach (var navigation in navigations)
                 {
-                    foreach (var navigation in navigations)
+                    var navigationEntityType = Context.Model.FindEntityType(navigation.ClrType);
+                    if (navigationEntityType != null)
                     {
-                        var navigationEntityType = Context.Model.FindEntityType(navigation.ClrType);
-                        if (navigationEntityType != null)
+                        prop = navigationEntityType.ClrType.GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                            .FirstOrDefault(p => string.Equals(p.Name, sortProperty, StringComparison.OrdinalIgnoreCase));
+                        if (prop != null)
                         {
-                            prop = navigationEntityType.ClrType.GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                                .FirstOrDefault(p => string.Equals(p.Name, sortProperty, StringComparison.OrdinalIgnoreCase));
-                            if (prop != null)
-                            {
-                                // Cria uma expressão para acessar a propriedade da entidade de navegação
-                                var parameter = Expression.Parameter(typeof(T), "x");
-                                var navigationProperty = Expression.Property(parameter, navigation.Name);
-                                var property = Expression.Property(navigationProperty, prop.Name);
-                                var converted = Expression.Convert(property, typeof(object));
-                                sortExpression = Expression.Lambda<Func<T, object>>(converted, parameter);
-                                break;
-                            }
+                            // Cria uma expressão para acessar a propriedade da entidade de navegação
+                            var parameter = Expression.Parameter(typeof(T), "x");
+                            var navigationProperty = Expression.Property(parameter, navigation.Name);
+                            var property = Expression.Property(navigationProperty, prop.Name);
+                            var converted = Expression.Convert(property, typeof(object));
+                            sortExpression = Expression.Lambda<Func<T, object>>(converted, parameter);
+                            break;
                         }
                     }
                 }
             }
+        }
+        else if (prop != null)
+        {
+            var parameter = Expression.Parameter(typeof(T), "x");
+            var property = Expression.Property(parameter, prop.Name);
+            var converted = Expression.Convert(property, typeof(object));
+            sortExpression = Expression.Lambda<Func<T, object>>(converted, parameter);
         }
 
         // Se a propriedade ainda não for encontrada, usa a primeira propriedade pública da entidade T
@@ -95,25 +169,5 @@ public abstract class BaseRepository<T> where T : class, new()
         }
 
         return list;
-    }
-
-    public virtual T GetById(Guid id)
-    {
-        return Context.Set<T>().Find(id) ?? new();
-    }
-
-    public virtual T GetById(int id)
-    {
-        return Context.Set<T>().Find(id) ?? new();
-    }
-
-    public virtual IEnumerable<T> Find(Expression<Func<T, bool>> expression)
-    {
-        return Context.Set<T>().Where(expression);
-    }
-
-    public virtual bool Exists(Expression<Func<T, bool>> expression)
-    {
-        return Find(expression).Any();
     }
 }
